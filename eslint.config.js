016cdd194473cef7c9dacde81819976ae3787bf2
wangzhichang <18329110825@163.com>
2024-08-09 09:13:00 +0800

    chore: update eslint configuration file

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
+//
 export default tseslint.config({
 	extends: [js.configs.recommended, ...tseslint.configs.recommended],
 	files: ['**/*.{ts,tsx}'],
