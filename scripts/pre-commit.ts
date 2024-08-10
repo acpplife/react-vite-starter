d2eba5966ca6e0e76c222acf79a93ca1fbf93d3d
wangzhichang <18329110825@163.com>
2024-08-10 11:00:39 +0800

    feat: ✨  1

---
+++ scripts/pre-commit.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env zx
+/* eslint-disable no-console */
+
+import { $ } from 'zx'
+
+console.log('开始执行代码质量评估...\n')
+
+await import('./check').catch((out) => {
+	console.log(out)
+	throw new Error('代码质量评估失败, 请检查代码')
+})
+
+console.log('printf "检测通过, 创建 commit 中...\n')
+
+await $`git add .`
---
+++ scripts/utils.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+
+import { ProcessOutput } from 'zx/core'
+
+export function printObject(
+	object: Record<string, unknown> | ProcessOutput,
+	method: 'log' | 'warn' | 'error' = 'log'
+) {
+	for (const [key, value] of Object.entries(object)) {
+		console[method](`${key}:\n${value}\n`)
+	}
+}
